010f566e61744ac47ba0f0be4a8a96a70d4c78fc
mappinjack <jack@jackforsyth.com>
2018-11-19 22:20:32 -0500

    Initial commit of bird quiz

---
+++ scripts/birddict.js
@@ -0,0 +1,17 @@
+birdDict = {
+    "americancrow": {
+        "name": "American crow",
+        "alternatenames": ["crow"],
+        "images": ["americancrow.jpg"],
+        "sounds": [],
+        "latin": ""
+    },
+    "americanrobin": {
+        "name": "American robin",
+        "alternatenames": ["robin"],
+        "images": ["americanrobin.jpg"],
+        "sounds": [],
+        "latin": ""
+    }
+
+}
