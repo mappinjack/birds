d13fe8c871c68d74b6ff685eae648484b94503aa
mappinjack <jack@jackforsyth.com>
2018-11-22 21:51:06 -0500

    Expand bird db, add audio-only

--- scripts/birddict.js
+++ scripts/birddict.js
@@ -2,16 +2,154 @@ birdDict = {
     "americancrow": {
         "name": "American crow",
         "alternatenames": ["crow"],
-        "images": ["americancrow.jpg"],
-        "sounds": [],
-        "latin": ""
+        "images": [{
+            "name": "americancrow.jpg",
+            "source": "https://www.flickr.com/photos/wildreturn/24244535799/"
+        },
+        {
+            "name": "americancrow2.jpg",
+            "source": "https://www.flickr.com/photos/wildreturn/24503957912/"
+        },
+        {
+            "name": "americancrow3.jpg",
+            "source": "https://www.flickr.com/photos/wildreturn/24244528299/"
+        }
+    ],
+        "sounds": ["americancrow.mp3"],
+        "latin": "Corvus brachyrhynchos",
+        "more": "https://www.allaboutbirds.org/guide/American_Crow/overview"
     },
     "americanrobin": {
         "name": "American robin",
         "alternatenames": ["robin"],
-        "images": ["americanrobin.jpg"],
-        "sounds": [],
-        "latin": ""
+        "images": [{
+                "name": "americanrobin.jpg",
+                "source": "https://www.flickr.com/photos/marknenadov/33218315482/"
+            },
+            {
+                "name": "americanrobin2.jpg",
+                "source": "https://commons.wikimedia.org/wiki/User:Ltshears"
+            },
+            {
+                "name": "americanrobin3.jpg",
+                "source": "https://www.flickr.com/photos/cricketsblog/38559904200/"
+            }
+        ],
+        "sounds": ["americanrobin.mp3"],
+        "latin": "Turdus migratorius",
+        "more": "https://www.allaboutbirds.org/guide/american_robin"
+    },
+
+    "blackcappedchickadee": {
+        "name": "Black-capped chickadee",
+        "alternatenames": ["chickadee", "chikadee", "black capped chickadee"],
+        "images": [{
+                "name": "blackcappedchickadee.jpg",
+                "source": "https://www.flickr.com/photos/qmnonic/2397568028/"
+            },
+            {
+                "name": "blackcappedchickadee2.jpg",
+                "source": "https://www.flickr.com/photos/usfwsmtnprairie/15290761526/"
+            },
+            {
+                "name": "blackcappedchickadee3.jpg",
+                "source": "https://www.flickr.com/photos/7232133@N08/4391247828"
+            }
+        ],
+        "sounds": ["blackcappedchickadee.mp3"],
+        "latin": "Poecile atricapillus",
+        "more": "https://www.allaboutbirds.org/guide/Black-capped_Chickadee/overview"
+    },
+
+    "northerncardinal": {
+        "name": "Northern cardinal",
+        "alternatenames": ["redbird", "cardinal", "common cardinal"],
+        "images": [{
+                "name": "northerncardinal.jpg",
+                "source": "https://www.flickr.com/photos/9765210@N03/2587737594/"
+            },
+            {
+                "name": "northerncardinal2.jpg",
+                "source": "https://commons.wikimedia.org/w/index.php?title=User:Torindkflt&action=edit&redlink=1"
+            },
+            {
+                "name": "northerncardinal3.jpg",
+                "source": "http://geoffclarke.hubpages.com/hub/Canadian-Birds-in-my-Garden"
+            }
+        ],
+        "sounds": ["northerncardinal.mp3"],
+        "latin": "Cardinalis cardinalis",
+        "more": "https://www.allaboutbirds.org/guide/Northern_Cardinal/overview"
+    },
+    "bluejay": {
+        "name": "Blue jay",
+        "alternatenames": ["jaybird", "bluejay"],
+        "images": [{
+                "name": "bluejay.jpg",
+                "source": "https://commons.wikimedia.org/wiki/User:Mdf"
+            },
+            {
+                "name": "bluejay2.jpg",
+                "source": "https://www.flickr.com/photos/31064702@N05/4205227030/"
+            },
+            {
+                "name": "bluejay3.jpeg",
+                "source": "https://500px.com/jocelynm"
+            }
+        ],
+        "sounds": ["bluejay.mp3"],
+        "latin": "Cyanocitta cristata",
+        "more": "https://www.allaboutbirds.org/guide/Blue_Jay/overview"
+    },
+
+    "redwingedblackbird": {
+        "name": "Red-winged blackbird",
+        "alternatenames": ["red winged blackbird", "redwinged blackbird", "red-winged black-bird", "red winged black-bird", "red winged black bird", "redwinged black-bird"],
+        "images": [{
+                "name": "redwingedblackbird.jpg",
+                "source": "https://commons.wikimedia.org/w/index.php?title=User:Bmajoros&action=edit&redlink=1"
+            },
+            {
+                "name": "redwingedblackbird2.jpg",
+                "source": "https://commons.wikimedia.org/wiki/User:Cephas"
+            },
+            {
+                "name": "redwingedblackbird3.jpg",
+                "source": "https://www.flickr.com/photos/wildreturn/34352785510/"
+            }
+        ],
+        "sounds": ["redwingedblackbird.mp3"],
+        "latin": "Agelaius phoeniceus",
+        "more": "https://www.allaboutbirds.org/guide/Red-winged_Blackbird/overview"
     }
+
+
+
+
+
+}
+
+empty =     {
+    
+    "empty": {
+    "name": "",
+    "alternatenames": [""],
+    "images": [{
+            "name": "",
+            "source": ""
+        },
+        {
+            "name": "",
+            "source": ""
+        },
+        {
+            "name": "",
+            "source": ""
+        }
+    ],
+    "sounds": [""],
+    "latin": "",
+    "more": ""
+}
 
 }
